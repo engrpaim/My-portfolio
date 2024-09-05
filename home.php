60bbd3655f60087253390f0c1d91c3d4e1d9bede
engrpaim <joseephraimbatacan@gmail.com>
2024-09-05 18:00:22 +0800

    Portfolio part1

---
+++ home.php
@@ -0,0 +1,8 @@
+<div id="homeContainer">
+    <h1 id="homeName">Juan Dela Cruz</h1>
+    <p>I’m Juan Dela Cruz, a web developer who turns ideas into stunning websites.
+        <br />Dive into my portfolio to see how I blend creativity with technical skill to build seamless digital experiences.
+    </p>
+
+    <p>Let’s create something amazing together!</p>
+</div>
